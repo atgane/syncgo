8dbca4de2b52e4933edcab7c03cfdca332c4e2a4
atgane <hyper201286@gmail.com>
2024-11-09 22:52:23 +0900

    impl 02

--- 02/main.go
+++ 02/main.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	q := make(chan struct{}, 10)
-	q <- struct{}{}
-	q <- struct{}{}
-	q <- struct{}{}
-	q <- struct{}{}
+	listener, err := net.Listen("tcp", ":4999")
+	if err != nil {
+		fmt.Println("listen error", err)
+		return
+	}
 
-	close(q)
-	fmt.Println(len(q))
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("accept error", err)
+			continue
+		}
+
+		go func(conn net.Conn) {
+			b := make([]byte, 1024)
+			for {
+				r, err := conn.Read(b)
+				if err != nil {
+					fmt.Println("read error", err)
+					break
+				}
+
+				_, err = conn.Write(b[:r])
+				if err != nil {
+					fmt.Println("write error", err)
+					break
+				}
+			}
+		}(conn)
+	}
 }
